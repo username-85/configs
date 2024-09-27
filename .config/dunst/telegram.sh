71c6d84e63ec38405ddd62ab6b8be8acbd79a504
username-85 <void.hjkl@yandex.ru>
2024-09-27 13:36:22 +0700

    dunst: fix telegram notification date and time

--- .config/dunst/telegram.sh
+++ .config/dunst/telegram.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 if [ "$HOSTNAME" = "work" ]; then
-    date_str=$(date +"%F %R" -d @$DUNST_TIMESTAMP)
+    date_str=$(date +"%F %R")
     echo "$date_str - $DUNST_SUMMARY" > "$HOME/data/Mail/telegram/new/last"
 fi

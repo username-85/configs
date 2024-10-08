ab9b5cccf00448be1b7157c483bb7995c015ce1a
username-85 <void.hjkl@yandex.ru>
2024-10-08 23:49:12 +0700

    sh: update settings

--- .bin/misc/byedpi.sh
+++ .bin/misc/byedpi.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-ciadpi --port 1080 --disorder 1 --fake 0 --ttl 1 -A torst --tlsrec 1+s --debug 1
+#ciadpi --port 1080 --disorder 1 --fake 0 --ttl 1 -A torst --tlsrec 1+s --debug 1
+ciadpi --port 1080 --debug 2 --ip 127.0.0.1 --oob 1 --split 2

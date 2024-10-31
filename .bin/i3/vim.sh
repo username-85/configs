afb947dbaaa1e1610e4bc9c9defee71de02dce28
username-85 <void.hjkl@yandex.ru>
2024-10-31 17:12:24 +0700

    i3: enablel spell in vim.sh

--- .bin/i3/vim.sh
+++ .bin/i3/vim.sh
@@ -5,4 +5,4 @@ set -e
 #tmp_file=$(mktemp -u).text_scratchpad
 tmp_file="/tmp/vim.txt"
 
-st -c "__text_scratchpad" -e "$SHELL" -lc "sleep 0.1 && vim -c startinsert  ${tmp_file}" && xclip -selection clipboard < "$tmp_file"
+st -c "__text_scratchpad" -e "$SHELL" -lc "sleep 0.1 && vim -c startinsert -c 'Spell' ${tmp_file}" && xclip -selection clipboard < "$tmp_file"
